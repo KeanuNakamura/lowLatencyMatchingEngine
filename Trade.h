2720ee98d18c1353bf8aa439feae66bbfd5ba21f
Keanu Nakamura <keanunakamura@berkeley.edu>
2026-06-06 15:44:56 -0700

    Comparison for trades

--- Trade.h
+++ Trade.h
@@ -1,4 +1,4 @@
-ifndef TRADE_H
+#ifndef TRADE_H
 #define TRADE_H
 #include "Order.h"
 
@@ -7,5 +7,13 @@ struct Trade {
     OrderId incomingOrderId;
     Price price;
     Quantity quantity;     
+
+    bool operator==(const Trade& other) const {
+    return buy_order_id == other.buy_order_id &&
+           sell_order_id == other.sell_order_id &&
+           price == other.price &&
+           quantity == other.quantity;
 }
+
+};
 #endif
